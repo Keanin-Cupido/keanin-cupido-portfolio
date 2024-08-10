2f0549393663d22a5e4a3bbd01409483426d3262
Keanin Cupido <keanin16@gmail.com>
2024-08-10 16:42:04 +0200

    style: add contact section

--- my-web-portfolio/src/data/techStack.js
+++ my-web-portfolio/src/data/techStack.js
@@ -2,49 +2,57 @@ let techStack = [
     {
         icon: "react",
         name: "React.js",
-        description: "A popular JavaScript library for building user interfaces, known for its component-based architecture and efficient rendering with a virtual DOM.",
+        description:
+            "A popular JavaScript library for building user interfaces, known for its component-based architecture and efficient rendering with a virtual DOM.",
         btnInfo: ["Learn More", "https://react.dev/", true, true],
     },
     {
         icon: "astro",
         name: "Astro.js",
-        description: "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+        description:
+            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
     {
         icon: "astro",
         name: "Astro.js",
-        description: "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+        description:
+            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
     {
         icon: "astro",
         name: "Astro.js",
-        description: "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+        description:
+            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
     {
         icon: "react",
         name: "React.js",
-        description: "A popular JavaScript library for building user interfaces, known for its component-based architecture and efficient rendering with a virtual DOM.",
+        description:
+            "A popular JavaScript library for building user interfaces, known for its component-based architecture and efficient rendering with a virtual DOM.",
         btnInfo: ["Learn More", "https://react.dev/", true, true],
     },
     {
         icon: "astro",
         name: "Astro.js",
-        description: "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+        description:
+            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
     {
         icon: "astro",
         name: "Astro.js",
-        description: "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+        description:
+            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
     {
         icon: "astro",
         name: "Astro.js",
-        description: "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+        description:
+            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
 ];
