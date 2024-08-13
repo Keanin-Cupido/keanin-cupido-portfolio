1597c1e906bf11c6947808e2edde6fbfd2df3086
Keanin Cupido <keanin16@gmail.com>
2024-08-13 21:23:31 +0200

    update styles

--- my-web-portfolio/src/data/techStack.js
+++ my-web-portfolio/src/data/techStack.js
@@ -10,50 +10,50 @@ let techStack = [
         icon: "astro",
         name: "Astro.js",
         description:
-            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
+            "A modern static site generator that allows you to build websites with your favorite JS framework and automatically optimizes performance by shipping only what's needed to the browser.",
         btnInfo: ["Learn More", "https://astro.build/", true, true],
     },
     {
-        icon: "astro",
-        name: "Astro.js",
+        icon: "nextjs",
+        name: "Next.js",
+        description:
+            "Building powerful and scalable web applications with server-side rendering and routing.",
+        btnInfo: ["Learn More", "https://nextjs.org/", true, true],
+    },
+    {
+        icon: "tailwindcss",
+        name: "Tailwind",
         description:
             "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
-        btnInfo: ["Learn More", "https://astro.build/", true, true],
+        btnInfo: ["Learn More", "https://tailwindcss.com/", true, true],
     },
     {
-        icon: "astro",
-        name: "Astro.js",
-        description:
-            "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
-        btnInfo: ["Learn More", "https://astro.build/", true, true],
-    },
-    {
-        icon: "react",
-        name: "React.js",
+        icon: "nodejs",
+        name: "Node.js",
         description:
             "A popular JavaScript library for building user interfaces, known for its component-based architecture and efficient rendering with a virtual DOM.",
-        btnInfo: ["Learn More", "https://react.dev/", true, true],
+        btnInfo: ["Learn More", "https://nodejs.org/en", true, true],
     },
     {
-        icon: "astro",
-        name: "Astro.js",
+        icon: "typescript",
+        name: "TypeScript",
         description:
             "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
-        btnInfo: ["Learn More", "https://astro.build/", true, true],
+        btnInfo: ["Learn More", "https://www.typescriptlang.org/", true, true],
     },
     {
-        icon: "astro",
-        name: "Astro.js",
+        icon: "supabase",
+        name: "Supabase",
         description:
             "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
-        btnInfo: ["Learn More", "https://astro.build/", true, true],
+        btnInfo: ["Learn More", "https://supabase.com/", true, true],
     },
     {
-        icon: "astro",
-        name: "Astro.js",
+        icon: "vscode",
+        name: "VS Code",
         description:
             "A frontend framework designed for building fast, content-focused sites by optimizing server-side rendering & delivering minimal client-side JS.",
-        btnInfo: ["Learn More", "https://astro.build/", true, true],
+        btnInfo: ["Learn More", "https://code.visualstudio.com/", true, true],
     },
 ];
 
