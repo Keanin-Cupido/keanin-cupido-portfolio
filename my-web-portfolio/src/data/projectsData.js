620c8050e8430ce6c84fa7de906b37432068bfa1
Keanin Cupido <keanin16@gmail.com>
2024-08-17 11:08:15 +0200

    fix: navbar styles

---
+++ my-web-portfolio/src/data/projectsData.js
@@ -0,0 +1,46 @@
+const projectsData = [
+    {
+        title: "Content Writing Website",
+        summary:
+            "Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis.",
+        techStack: ["react", "supabase", "tailwindcss"],
+        projectImgName: "default.jpg",
+        projectImgAlt: "project",
+        projectImgW: 475,
+        projectImgH: 156,
+        githubLink: "https://github.com/Keanin-Cupido",
+        liveLink: "https://github.com/Keanin-Cupido",
+        featured: true,
+        projectsPage: true,
+    },
+    {
+        title: "Content Writing Website",
+        summary:
+            "Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis.",
+        techStack: ["react", "supabase", "tailwindcss"],
+        projectImgName: "default.jpg",
+        projectImgAlt: "project",
+        projectImgW: 475,
+        projectImgH: 156,
+        githubLink: "https://github.com/Keanin-Cupido",
+        liveLink: "https://github.com/Keanin-Cupido",
+        featured: false,
+        projectsPage: true,
+    },
+    {
+        title: "Content Writing Website",
+        summary:
+            "Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis.",
+        techStack: ["react", "supabase", "tailwindcss"],
+        projectImgName: "default.jpg",
+        projectImgAlt: "project",
+        projectImgW: 475,
+        projectImgH: 156,
+        githubLink: "https://github.com/Keanin-Cupido",
+        liveLink: "https://github.com/Keanin-Cupido",
+        featured: false,
+        projectsPage: true,
+    },
+];
+
+export default projectsData;
