de042e927a61fbb27cd6ae164b71b5e60e545441
Keanin Cupido <keanin16@gmail.com>
2024-08-18 20:59:49 +0200

    fix adapter error

--- my-web-portfolio/astro.config.mjs
+++ my-web-portfolio/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    integrations: [tailwind(), icon()],
+  output: "server",
+  integrations: [tailwind(), icon()],
+  adapter: vercel()
 });
